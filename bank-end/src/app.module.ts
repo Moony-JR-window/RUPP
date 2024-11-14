699d332cb76b6c108f35c58142d4366689e57996
mony.rorn <mony.rorn@wingbank.com.kh>
2024-11-14 22:15:01 +0700

    config@0.0.1

--- bank-end/src/app.module.ts
+++ bank-end/src/app.module.ts
@@ -1,27 +1,40 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, Logger, OnModuleInit } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import configs from './feature-module/configs/configs';
+// import { UserSchema } from './feature-module/schamas/hello.schema';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: configs.database, // Path to .env file
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async () =>{
+        const uri = configs.database;;
+        await mongoose.connect (uri);
+        return {uri};
+      }, 
+    }),
+    // MongooseModule.forFeature([{ name: 'User', schema:UserSchema }]),
+  ],
+  providers:[],
+  controllers: [],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  private readonly logger = new Logger(AppModule.name);
 
-
-// // src/app.module.ts
-// import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { UserModule } from './user/user.module';
-
-// @Module({
-//   imports: [
-//     MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/yourdatabase', {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }),
-//     UserModule, // Feature module
-//   ],
-// })
-// export class AppModule {}
+  async onModuleInit() {
+    try {
+      if (mongoose.connection.readyState === 1) {
+        this.logger.log('MongoDB connected successfully');
+      } else {
+        this.logger.warn('MongoDB is not connected yet');
+      }
+    } catch (error) {
+      this.logger.error('Error during MongoDB connection setup:', error);
+    }
+  }
+}
