b096b6061826ba7b783d281c1a861d5939640a97
mony.rorn <mony.rorn@wingbank.com.kh>
2025-01-18 18:51:16 +0700

    time@0.0.4

--- front-end/app/component/menu/Profile.tsx
+++ front-end/app/component/menu/Profile.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { IoHomeSharp } from "react-icons/io5";
 import { IoSettings } from "react-icons/io5";
 import { FaLock } from "react-icons/fa";
-import { MdLogout } from "react-icons/md";
 import Image from 'next/image';
 import profile from "/app/image/profile.jpg"
 import TimeDisplay from '../time/Time';
+import Logout from './Logout';
 
 const Profile = () => {
     return (
         <div className=' w-[25%] h-screen flex flex-col justify-center items-center bg-blue-400 '>
             <div className=' flex flex-col gap-10 justify-start items-start h-full  '>
-                <div className=' font-extrabold container pl-4 pt-5 text-2xl '>
+                <div className=' font-serif container pl-4 pt-5 text-2xl '>
                     {<TimeDisplay />}
                 </div>
                 <div className='p-10 flex flex-col gap-5  '>
@@ -44,9 +44,7 @@ const Profile = () => {
                     </div>
                 </div>
 
-                <div className=' font-extrabold container w-full pt-10 text-white ' >
-                    <span className='flex justify-start items-center  '> <span className='bg-blue-800 p-2 flex justify-center items-center rounded-full ' ><MdLogout /></span><label className='p-3'>Logout</label></span>
-                </div>
+                <Logout />
             </div>
 
         </div>
