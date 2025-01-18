896468b6bbf37529545808a62302cfc838e004d8
mony.rorn <mony.rorn@wingbank.com.kh>
2025-01-18 20:52:09 +0700

    menu@0.0.3

--- front-end/app/component/main/Main.tsx
+++ front-end/app/component/main/Main.tsx
@@ -6,7 +6,7 @@ import { toggle } from '../interface/Toggle'
 
 const Main:React.FC<toggle> = ({toggle,setToggle}) => {
     return (
-        <div className={` w-full h-screen justify-start items-center flex flex-col bg-red-400 ${toggle? " ml-[-50px]  rounded-s-3xl " : " delay-[1000ms] "} `}>
+        <div className={` w-full h-screen justify-start items-center flex flex-col bg-gray-200 ${toggle? " ml-[-50px]  rounded-s-3xl " : " delay-[1000ms] "} `}>
             <ToggleMenu toggle={toggle} setToggle={setToggle}  />
             Main
 
--- front-end/app/component/menu/ToggleMenu.tsx
+++ front-end/app/component/menu/ToggleMenu.tsx
@@ -9,7 +9,7 @@ const ToggleMenu:React.FC<toggle> = ({toggle,setToggle}) => {
     return (
         <div className='flex p-5 pt-10 w-full justify-start items-start '>
             <div className=' pl-5 w-2/12 mt-[-10px] '>
-                <h1 className='text-4xl font-semibold '> My Smart <br/> Home </h1>
+                <h1 className='text-4xl font-semibold '>Dentist management <br/> system </h1>
             </div>
             <ToggleAnimation toggle={toggle} setToggle={setToggle}  />
         </div>
