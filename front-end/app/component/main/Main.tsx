b767b71603a21154bf6841759a71b8f572829742
mony.rorn <mony.rorn@wingbank.com.kh>
2025-01-18 20:57:19 +0700

    menu@0.0.3

--- front-end/app/component/main/Main.tsx
+++ front-end/app/component/main/Main.tsx
@@ -4,7 +4,7 @@ import { toggle } from '../interface/Toggle'
 
 const Main:React.FC<toggle> = ({toggle,setToggle}) => {
     return (
-        <div className={` w-full h-screen justify-start items-center flex flex-col bg-gray-200 ${toggle? " ml-[-50px]  rounded-s-3xl " : " delay-[1000ms] "} `}>
+        <div className={` w-full h-screen justify-start items-center flex flex-col bg-gray-200 ${toggle? " ml-[-2%]  rounded-s-3xl " : " delay-[1000ms] "} `}>
             <ToggleMenu toggle={toggle} setToggle={setToggle}  />
             Main
 
